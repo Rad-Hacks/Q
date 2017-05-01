3bdff7f4697469aec2abb7a689e7dad29d602e43
Chih-Yun Doris Chiu <chihyundorischiu@gmail.com>
2017-05-01 10:34:04 -0500

    Update schema.sql
    add in 6 more dummy-data

--- schema.sql
+++ schema.sql
@@ -19,3 +19,9 @@ CREATE TABLE events(
 );
 
 INSERT INTO events (name, amount, address, city, state, date, time, duration, contactEmail) VALUES ("Franklin's", 200, '900 E. 11th St.', 'Austin', 'TX', '2017-05-05', '11:30:00', 4, 'peter@johnson.com');
+INSERT INTO events (name, amount, address, city, state, date, time, duration, contactEmail) VALUES ("Abby's", 40, '9900 E. 11th St.', 'Dallas', 'TX', '2017-05-05', '11:30:00', 4, 'peter@johnson.com');
+INSERT INTO events (name, amount, address, city, state, date, time, duration, contactEmail) VALUES ("Fred's", 10, '800 E. 11th St.', 'Dallas', 'TX', '2017-05-05', '11:30:00', 4, 'peter@johnson.com');
+INSERT INTO events (name, amount, address, city, state, date, time, duration, contactEmail) VALUES ("Tom's", 150, '4000 E. 11th St.', 'Orlando', 'FL', '2017-05-05', '11:30:00', 4, 'peter@johnson.com');
+INSERT INTO events (name, amount, address, city, state, date, time, duration, contactEmail) VALUES ("Billy's", 270, '400 E. 11th St.', 'Houston', 'TX', '2017-05-05', '11:30:00', 4, 'peter@johnson.com');
+INSERT INTO events (name, amount, address, city, state, date, time, duration, contactEmail) VALUES ("Kate's", 50, '30 E. 11th St.', 'Orlando', 'FL', '2017-05-05', '11:30:00', 4, 'peter@johnson.com');
+INSERT INTO events (name, amount, address, city, state, date, time, duration, contactEmail) VALUES ("Sally's", 125, '100 E7. 11th St.', 'Austin', 'TX', '2017-05-05', '11:30:00', 4, 'peter@johnson.com');
