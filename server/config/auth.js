b54ff7c2eeb5b681f0cc66bcf572f06970d504c0
Doris <chihyundorischiu@gmail.com>
2017-05-04 18:01:38 -0500

    google auth client ID and secret

---
+++ server/config/auth.js
@@ -0,0 +1,7 @@
+module.esports = {
+  googleAuth: {
+    clientID: '1031010390104-f139vsdq3f8dn21usnuj4h3jtq8jpdpf.apps.googleusercontent.com',
+    clientSecret: 'gpLhyviai6ibruJz59EWEZn7',
+    callbackURL: 'http://127.0.0.1:8080/auth/google/callback',
+  },
+};
