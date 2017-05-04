95e171e0f9360abaf623ce1422ae0639fb12af3b
Doris <chihyundorischiu@gmail.com>
2017-05-03 20:42:51 -0500

    (fix) sort doesn't sort original array-> sorts copy to keep original order

--- client/src/components/EventsList.jsx
+++ client/src/components/EventsList.jsx
@@ -17,17 +17,18 @@ class EventsList extends Component {
           <p> Here are your local events: </p>
           {this.props.events.filter(event =>
             (`${event.city}, ${event.state}`) === this.props.currentLocation)
-          .sort((a, b) => a.amount - b.amount)
+          .sort((a, b) => b.amount - a.amount)
           .map(event => (
             <EventsListItem key={event.id} style={{ width: '200px' }} data={event} />
           ))}
         </div>
       );
     } else if (this.props.sort) {
+      const eventQs = this.props.events.slice();
       return (
         <div>
           <p> Please search above to find events in your area: </p>
-          {this.props.events.sort((a, b) => b.amount - a.amount)
+          {eventQs.sort((a, b) => b.amount - a.amount)
           .map(event => (
             <EventsListItem key={event.id} style={{ width: '200px' }} data={event} />
           ))}
