7c2419e7bb7ad62d92e50d9c914834a61b75f4b0
Doris <chihyundorischiu@gmail.com>
2017-05-04 18:08:37 -0500

    (feat) googleAuth passport--google-oauth

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
-// const path = require('path');
+const path = require('path');
 // const router = require('router');
 const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strate;
 const flash = require('connect-flash');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
@@ -10,6 +11,42 @@ const session = require('express-session');
 const crypto = require('crypto');
 const bcrypt = require('bcrypt');
 const db = require('../db/index.js');
+const authConfig = require('./config/passport')(passport);
+
+
+//   For persistent logins with sessions, Passport needs to serialize users into
+//   and deserialize users out of the session. Typically, this is as simple as
+//   storing the user ID when serializing, and finding the user by ID when
+//   deserializing.
+passport.serializeUser((user, done) => {
+  // done(null, user.id);
+  done(null, user);
+});
+
+passport.deserializeUser((obj, done) => {
+  done(null, obj);
+});
+
+// Use the GoogleStrategy within Passport.
+//   Strategies in Passport require a `verify` function, which accept
+//   credentials (in this case, an accessToken, refreshToken, and Google
+//   profile), and invoke a callback with a user object.
+//   See http://passportjs.org/docs/configure#verify-callback
+passport.use(new GoogleStrategy(
+  // Use the API access settings stored in ./config/auth.json. You must create
+  // an OAuth 2 client ID and secret at: https://console.developers.google.com
+  authConfig.googleAuth,
+  (accessToken, refreshToken, profile, done) => {
+    // example with mongodb---> need to find user in db or create if not exists,
+    // Typically you would query the database to find the user record
+    // associated with this Google profile, then pass that object to the `done`
+    // callback.
+    // User.findOrCreate({ googleId: profile.id }, (err, user) => (
+    //   done(err, user)
+    // )
+    return done(null, profile);
+  },
+));
 
 const app = express();
 app.use(morgan('dev')); // log every request to the console
@@ -21,13 +58,14 @@ app.use(bodyParser.urlencoded({
 
 app.use(session({
   secret: 'iloveqs',
-  resave: true,
+  resave: false,
   saveUninitialized: true,
 }));
+
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
-
+app.use(express.static(path.join(__dirname, '/../server/client/public')));
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
@@ -93,6 +131,15 @@ app.post('/api/users', (req, res) => {
   });
 });
 
+app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+app.get('/auth/google/callback',
+  passport('google', {
+    failuredirect: '/login',
+  }),
+  (req, res) => {
+    // Authenticated successfully
+    res.redirect('/');
+  });
 
 app.listen(8080, () => {
   console.log('Listening on port 8080!');
