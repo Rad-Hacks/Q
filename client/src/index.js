43baa5444b29cee7b7f4f727c9e34d246b7ccb89
Doris <chihyundorischiu@gmail.com>
2017-05-05 11:23:24 -0500

    (cleanup) eslinting errors in index.js file

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import App from './App';
 import Signup from './components/Signup';
 import Login from './components/Login';
@@ -10,11 +10,11 @@ import './index.css';
 ReactDOM.render(
   <Router>
     <div>
-        <Route exact path='/' component={App} />
-        <Route path='/login' component={Login} />
-        <Route path='/signup' component={Signup} />
-        <Route path='/welcome' component={Welcome} />
+      <Route exact path="/" component={App} />
+      <Route path="/login" component={Login} />
+      <Route path="/signup" component={Signup} />
+      <Route path="/welcome" component={Welcome} />
     </div>
-  </Router>  ,
+  </Router>,
   document.getElementById('root')
 );
