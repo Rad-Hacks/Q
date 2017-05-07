5737759348f3509e4f7ed7231343459161447149
Rodolfo Rodriguez <rodolfobgibson@gmail.com>
2017-05-07 12:02:08 -0500

    Post to server on user log in

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -10,13 +10,28 @@ class Login extends Component {
     };
   }
 
+  handleSubmit() {
+    fetch('http://localhost:8080/api/login', {
+      method: 'post',
+      body: JSON.stringify({
+        username: this.state.username,
+        password: this.state.password,
+      }),
+    });
+  }
   render() {
     return (
       <form className="login">
         <p className="title">Log in</p>
-        <input type="text" placeholder="Username" autoFocus />
+        <input
+          type="text" placeholder="Username" autoFocus
+          onChange={(e) => { this.setState({ username: e.target.value }); }}
+        />
         <i className="fa fa-user" />
-        <input type="password" placeholder="Password" />
+        <input
+          type="password" placeholder="Password"
+          onChange={(e) => { this.setState({ password: e.target.value }); }}
+        />
         <i className="fa fa-key" />
         <button onClick={() => this.handleSubmit}>
           <i className="spinner" />
