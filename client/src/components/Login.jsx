a271342d176029cf099c5dbfc3cc4d13ca99fc81
Rodolfo Rodriguez <rodolfobgibson@gmail.com>
2017-05-07 11:35:42 -0500

    Added fields to login state

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,25 +1,34 @@
 import React, { Component } from 'react';
-import $ from 'jquery';
 
 class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: '',
+      city: '',
+      state: '',
+      phone: '',
+      email: '',
+    };
+  }
 
   render() {
     return (
       <form className="login">
         <p className="title">Log in</p>
-        <input type="text" placeholder="Email" />
-        <i className="fa fa-envelope" />
         <input type="text" placeholder="Username" autoFocus />
         <i className="fa fa-user" />
         <input type="password" placeholder="Password" />
         <i className="fa fa-key" />
-        <input type="text" placeholder="City" />
+        <input type="password" placeholder="City" />
         <i className="fa fa-location-arrow" />
         <input type="text" placeholder="State" />
-        <i className="fa fa-location-arrow" />
-        <input type="text" placeholder="Phone" />
+        <i className="fa fa-location-arroe" />
+        <input type="password" placeholder="Phone" />
         <i className="fa fa-phone" />
-
+        <input type="password" placeholder="Email" />
+        <i className="fa fa-email" />
         <button onClick={() => this.handleSubmit}>
           <i className="spinner" />
           <span className="state">Log in</span>
