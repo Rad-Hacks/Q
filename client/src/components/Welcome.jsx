e297095e48776f399d27bf3856dc44ab1726f6f4
Doris <chihyundorischiu@gmail.com>
2017-05-07 16:22:40 -0500

    (UI) center the signup and Q pages

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import Signup from './Signup';
+import welcomeimage from './../WelcomeImage.png';
 
 const Welcome = () => (
-  <div style={{ width: '100%', height: '100%' }}>
-    <Signup style={{ width: 320, position: 'absolute', left: '50%' }} />
-    <video autoPlay loop src="https://s3-us-west-1.amazonaws.com/q-assets/Shoes-Stories.mp4" type="video/mp4" />
+  <div className="container">
+    <article style={{ position: 'absolute', width: '100%', height: '100%' }}>
+      <h1 style={{ 'font-size': '50px', color: 'white', 'text-shadow': '-1px 0 black, 0 1px black, 1px 0 black, 0 -1px black' }}>
+        Welcome to Q!
+      </h1>
+      <Signup style={{ width: 320, position: 'absolute', left: '50%' }} />
+    </article>
+    <div className="fullscreen-bg">
+      <video autoPlay loop className="fullscreen-bg__video" poster={welcomeimage}>
+        <source src="https://s3-us-west-1.amazonaws.com/q-assets/Shoes-Stories.mp4" type="video/mp4" />
+      </video>
+    </div>
   </div>
 );
 
