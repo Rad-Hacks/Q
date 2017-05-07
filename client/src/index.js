fd6ebe4ad8f75e8d890fff0954940fc9e1d55edd
Rodolfo Rodriguez <rodolfobgibson@gmail.com>
2017-05-07 15:37:12 -0500

    Add home component

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Home from './Home';
+import Home from './components/Home';
 import Signup from './components/Signup';
 import Login from './components/Login';
 import Welcome from './components/Welcome';
