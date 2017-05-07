6f3675ae9f729c2ac7df894848b6371ca5e344ca
Rodolfo Rodriguez <rodolfobgibson@gmail.com>
2017-05-07 11:12:09 -0500

    Add Login component to Welcome page

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
+import Login from './Login.jsx';
 
 const Welcome = () => (
-  <div class="homepage-hero-module">
-    <div class="video-container">
-        <div class="filter" />
-        <video autoPlay loop class="fillWidth">
-            <source src="https://s3-us-west-1.amazonaws.com/q-assets/Shoes-Stories.mp4" type="video/mp4" />
-        </video>
-    </div>
-</div>
+  <div style={{ width: '100%', height: '100%' }}>
+    <Login style={{ width: 320, position: 'absolute', left: '50%' }} />
+    <video autoPlay loop src="https://s3-us-west-1.amazonaws.com/q-assets/Shoes-Stories.mp4" type="video/mp4" />
+  </div>
 );
 
 export default Welcome;
