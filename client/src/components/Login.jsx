f13099060bd4ba2541621fd4707d2027d13b19b3
Rodolfo Rodriguez <rodolfobgibson@gmail.com>
2017-05-07 10:37:12 -0500

    Redesign Login component

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,121 +1,44 @@
 import React, { Component } from 'react';
-import ReactCSSTG from 'react-addons-css-transition-group';
-import PropTypes from 'prop-types';
-import './Login.css';
+import $ from 'jquery';
 
-// Login component
 class Login extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isVisible: true,
-    };
-    // Bindings
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleRemount = this.handleRemount.bind(this);
-  }
-
-  handleSubmit(e) {
-    e.preventDefault();
-    this.setState({
-      isVisible: false,
+  componentDidMount() {
+    let working = false;
+    $('.login').on('submit', function(e) {
+      e.preventDefault();
+      if (working) return;
+      working = true;
+      var $this = $(this),
+        $state = $this.find('button > .state');
+      $this.addClass('loading');
+      $state.html('Authenticating');
+      setTimeout(function() {
+        $this.addClass('ok');
+        $state.html('Welcome back!');
+        setTimeout(function() {
+          $state.html('Log in');
+          $this.removeClass('ok loading');
+          working = false;
+        }, 4000);
+      }, 3000);
     });
-    return false;
   }
-  handleRemount(e) {
-    this.setState({
-      isVisible: true,
-    });
-    e.preventDefault();
-  }
-
   render() {
-    // const for React CSS transition declaration
-    const component = this.state.isVisible ? <Modal onSubmit={this.handleSubmit} key="modal" />
-    : <ModalBack onClick={this.handleRemount} key="bringitback" />;
-
     return (
-      <ReactCSSTG
-        transitionName="animation"
-        transitionAppear
-        transitionAppearTimeout={500}
-        transitionEnterTimeout={500}
-        transitionLeaveTimeout={300}
-      >
-        { component }
-      </ReactCSSTG>
+      <form className="login">
+        <p className="title">Log in</p>
+        <input type="text" placeholder="Username" autoFocus />
+        <i className="fa fa-user" />
+        <input type="password" placeholder="Password" />
+        <i className="fa fa-key" />
+        <button>
+          <i className="spinner" />
+          <span className="state">Log in</span>
+        </button>
+      </form>
     );
   }
+
 }
-
-// Modal
-const Modal = props => (
-  <div className="Modal">
-    <Logo />
-    <form onSubmit={props.onSubmit} >
-      <Input type="text" name="username" placeholder="username" />
-      <Input type="password" name="password" placeholder="password" />
-      <button> Login </button>
-    </form>
-    <div className="social-signin">
-      <button className="fb" onClick={props.onClick}>
-        <i className="fa fa-facebook" aria-hidden="true" />
-      </button>
-      <button className="tw" onClick={props.onClick}>
-        <i className="fa fa-twitter" aria-hidden="true" />
-      </button>
-    </div>
-    <a href="#/">Lost your password ?</a>
-  </div>
-);
-
-Modal.propTypes = {
-  onSubmit: PropTypes.function,
-  onClick: PropTypes.function,
-};
-
-// Generic input field
-const Input = props => (
-  <div className="Input">
-    <input
-      type={props.type}
-      name={props.name}
-      placeholder={props.placeholder}
-      required
-      autoComplete="false"
-    />
-    <label htmlFor={props.name} />
-  </div>
-);
-
-Input.propTypes = {
-  type: PropTypes.string,
-  name: PropTypes.string,
-  placeholder: PropTypes.placeholder,
-};
-
-// Fake logo
-const Logo = () => (
-  <div className="logo">
-    <i className="fa fa-bug" aria-hidden="true" />
-    <span> Log in to Q </span>
-  </div>
-);
-
-// Button to brind the modal back
-const ModalBack = props => (
-  <button
-    className="bringitback"
-    onClick={props.onClick}
-    key={props.className}
-  >
-  Bring the modal back!
-  </button>
-);
-
-ModalBack.propTypes = {
-  onClick: PropTypes.function,
-  className: PropTypes.string,
-};
 
 export default Login;
