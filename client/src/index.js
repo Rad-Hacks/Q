ff2a67642ac381f9bb8e39903ac028c076437c64
Rodolfo Rodriguez <rodolfobgibson@gmail.com>
2017-05-07 14:57:46 -0500

    Create an app component encapsulating Router

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './components/Home';
 import App from './App';
 import Signup from './components/Signup';
 import Login from './components/Login';
@@ -8,13 +9,15 @@ import Welcome from './components/Welcome';
 import './index.css';
 
 ReactDOM.render(
-  <Router>
-    <div>
-        <Route exact path='/' component={App} />
-        <Route path='/login' component={Login} />
-        <Route path='/signup' component={Signup} />
-        <Route path='/welcome' component={Welcome} />
-    </div>
-  </Router>  ,
+  <App>
+    <Router>
+      <div>
+          <Route exact path='/' component={Home} />
+          <Route path='/login' component={Login} />
+          <Route path='/signup' component={Signup} />
+          <Route path='/welcome' component={Welcome} />
+      </div>
+    </Router>
+  </App>,
   document.getElementById('root')
 );
